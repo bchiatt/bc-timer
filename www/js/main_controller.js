2237513dfc28a33becc63ce5816467e821da74c6
Brian Hiatt <brian.hiatt41@gmail.com>
2014-10-17 22:25:34 -0500

    final class work

--- www/js/main_controller.js
+++ www/js/main_controller.js
@@ -2,7 +2,39 @@
   'use-strict';
 
   angular.module('bc-timer')
-  .controller('MainCtrl', ['$scope', function($scope){
-    $scope.title = 'Timer';
+  .controller('MainCtrl', ['$scope', '$interval', function($scope, $interval){
+    $scope.stopped = true;
+    var timer;
+
+
+    $scope.clear = function(){
+      $scope.remain = 0;
+    };
+
+    $scope.start = function(){
+      $scope.stopped = false;
+      timer = $interval($scope.countdown, 1000);
+    };
+
+    $scope.stop = function(){
+      $scope.stopped = true;
+      $interval.cancel(timer);
+    };
+
+    $scope.add = function(int){
+      $scope.remain += int;
+    };
+
+    $scope.countdown = function(){
+      $scope.remain -= 1000;
+
+      if($scope.remain <= 0){
+        navigator.vibrate([100, 100, 100, 100, 100, 100, 100]);
+        $scope.stopped = true;
+        $interval.cancel(timer);
+      }
+    };
+
+    $scope.clear();
   }]);
 })();
